42937af4c24824f05b697ac52932a69266b3f345
Edilson Santos <erds-tecnologo@outlook.com.br>
2020-02-02 14:15:39 -0200

    Primeira versão do script

---
+++ Instalar_impressor.cmd
@@ -0,0 +1,56 @@
+@echo off
+rem AUTOR EDILSON RODRIGUES DOS DANTOS
+color A
+:INICIO
+cls
+echo.
+echo                     [DATA: %DATE%  HORA: %TIME%]
+echo.
+echo        ------------------------------------------------------
+echo        [HOSTNAME: %COMPUTERNAME%  USUARIO LOGADO: %USERNAME%]
+echo        ------------------------------------------------------
+@echo          _________________________________________________
+@echo          *************************************************
+@echo          ***            INSTALACAO DA IMPRESSORA       ***
+@echo          *************************************************
+@echo          _________________________________________________
+@echo.
+@echo                     ESCOLHA UMA DAS OPCOES ABAIXO
+@echo.
+@echo                     [1] - IMPRESSORA PRETO E BRANCO
+@echo                     [2] - IMPRESSORA COLORIDA
+@echo                     [3] - SAIR
+@echo.
+rem Comando que pega a opção que usuario digita e armazena na variavel 'opc'
+set /p opc=[Digite uma opcao:]
+rem Condição que verifica a opção escolhida pelo usuario e direciona para ação desejada.
+if %opc% == 1 goto PRETO_BRANCO 
+if %opc% == 2 goto COLORIDA
+if %opc% == 3 goto SAIR
+if %opc% GEQ 4 goto INICIO
+
+
+rem Codigo que faz a instalação da impressora colorida.
+:COLORIDA
+start \\10.102.80.61\PRNCOLORVMorumbi
+cls
+@echo.
+@echo                     INSTALACAO CONCLUIDA!!
+@echo.
+pause
+cls
+rem fim do codigo da instalação da impressora colorida e direciona para inicio do programa
+goto INICIO
+
+rem Codigo que faz a instalação da impressora colorida.
+:PRETO_BRANCO
+start \\10.102.80.61\PRNPBVMorumbi
+cls
+@echo.
+@echo                     INSTALACAO CONCLUIDA!!
+@echo.
+pause
+cls
+goto INICIO
+:SAIR
+exit'
